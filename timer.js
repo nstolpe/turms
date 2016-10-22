202e57c68583b128ea199aaf1b45afd08029abd9
nils <slincire@harpoonandhatchet.com>
2016-10-22 02:52:10 -0400

    fixed timer again

--- timer.js
+++ timer.js
@@ -1,43 +1,42 @@
 'use strict'
 
 const timer = function(duration, interval, onEnd, onInterval) {
-	function run(instance) {
-        if(instance.step++ == instance.steps) {
-			timeout = undefined;
-			instance.complete = true;
-            onEnd.call(instance);
-        } else {
-			let diff = (new Date().getTime() - startTime) - (step * speed);
-
-            if (onInterval) onInterval.call(instance);
-
-			instance.elapsed = new Date().getTime() - startTime;
-            timeout = setTimeout(function() { run(instance); }, (speed - diff));
-        }
-    }
-
-    var steps = (duration / 100) * (interval / 10),
+    let steps = (duration / 100) * (interval / 10),
         speed = duration / steps,
-        step = 0,
 		timeout,
-		startTime,
-		instance = {
-			duration: duration,
-			elapsed: 0,
-			complete: false,
-			step: 0,
-			steps: steps,
-			start() {
-				startTime = new Date().getTime();
-				step = 0;
-				this.elapsed = 0;
-				timeout = setTimeout(function() { run(instance); }, speed);
-			},
-			cancel() {
-				clearTimeout(timeout);
+		run = function(instance) {
+	        if(instance.step++ == instance.steps) {
 				timeout = undefined;
-			}
-		};
+				instance.complete = true;
+	            onEnd.call(instance);
+	        } else {
+				let diff = (new Date().getTime() - instance.startTime) - (instance.step * speed);
+
+	            if (onInterval) onInterval.call(instance);
+
+				instance.elapsed = new Date().getTime() - instance.startTime;
+	            timeout = setTimeout(function() { run(instance); }, (speed - diff));
+	        }
+	    };
+
+	const instance = {
+		startTime: null,
+		duration: duration,
+		elapsed: 0,
+		complete: false,
+		step: 0,
+		steps: steps,
+		start() {
+			this.startTime = new Date().getTime();
+			this.step = 0;
+			this.elapsed = 0;
+			timeout = setTimeout(function() { run(instance); }, speed);
+		},
+		cancel() {
+			clearTimeout(timeout);
+			timeout = undefined;
+		}
+	};
 
 	return instance;
 }
