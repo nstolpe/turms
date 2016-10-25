ea751f17b645b9a0122896bd6d2ebb3660b30e86
nils <slincire@gmail.com>
2016-10-24 20:52:38 -0400

    added handling of interval's that don't divide cleanly into a duration. added doc blocks

--- timer.js
+++ timer.js
@@ -1,25 +1,54 @@
 'use strict'
 
-const timer = function(duration, interval, onEnd, onInterval) {
-    // let steps = (duration / 100) * (interval / 10),
-    let steps = duration / interval,
-        speed = duration / steps,
-		timeout,
-		run = function(instance) {
-	        if(instance.step++ == instance.steps) {
-				instance.elapsed = new Date().getTime() - instance.startTime;
-				timeout = undefined;
-				instance.complete = true;
-	            onEnd.call(instance);
-	        } else {
-				let diff = (new Date().getTime() - instance.startTime) - (instance.step * speed);
+/**
+ * Sets up a self-adjusting timer. Based on code from the link below:
+ * https://www.sitepoint.com/creating-accurate-timers-in-javascript/
+ *
+ * @param duration    The full duration of the timer in ms.
+ *                    Once reached, `instance.end()` will be called.
+ *
+ * @param interval    The interval of time in ms after which the timer will.
+ *                    adjust itself to better match duration.
+ *                    2 seems to be the lower limit, 1 slow down the timer.
+ *
+ * @param onEnd       The function called when duration has expired.
+ *                     Without this the timer won't do anything noticeable.
+ *                    Called with `instance` set to `this`.
+ *
+ * @param onInterval  An optional function to be called on each step of the timer.
+ *                    Called with `instance` set to `this`.
+ *                    Calling `instance.cancel()` here doesn't work.
+ *
+ * @TODO Make this use `process.hrtime` instead of `new Date().getTime()` and compare
+ *       with epsilon before running next step for accuracy.
+ */
 
+const timer = function(duration, interval, onEnd, onInterval) {
+    let steps = Math.floor(duration / interval),
+		timeout,
+		fracStep = duration % interval !== 0 ? duration % interval : null,
+		run = function(instance) {
+			let diff;
+
+			instance.elapsed = new Date().getTime() - instance.startTime;
+			diff = instance.elapsed - (instance.step * interval);
+
+			if(instance.step === instance.steps) {
+				if (fracStep) timeout = setTimeout(function() { end(instance); }, (fracStep - diff));
+				else end(instance);
+	        } else {
+				instance.step++
 	            if (onInterval) onInterval.call(instance);
 
-				instance.elapsed = new Date().getTime() - instance.startTime;
-	            timeout = setTimeout(function() { run(instance); }, (speed - diff));
+	            timeout = setTimeout(function() { run(instance); }, (interval - diff));
 	        }
-	    };
+	    },
+		end = function(instance) {
+			timeout = undefined;
+			instance.elapsed = new Date().getTime() - instance.startTime;
+			instance.complete = true;
+			onEnd.call(instance);
+		};
 
 	const instance = {
 		startTime: null,
@@ -32,7 +61,7 @@ const timer = function(duration, interval, onEnd, onInterval) {
 			this.startTime = new Date().getTime();
 			this.step = 0;
 			this.elapsed = 0;
-			timeout = setTimeout(function() { run(instance); }, speed);
+			timeout = setTimeout(function() { run(instance); }, interval);
 		},
 		cancel() {
 			clearTimeout(timeout);
